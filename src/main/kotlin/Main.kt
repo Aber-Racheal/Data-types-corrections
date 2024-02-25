da051a60168e9ad78800b6901d8b4619b0f021ea
Aber-Racheal <rachealaberr@gmail.com>
2024-02-25 10:13:24 +0300

    Corrections for data type assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var firstName: String="Racheal"
+    var lastName: String="Aber"
+    var fullName:String= firstName+" " +lastName
+    println(fullName)
+
+    var age: Byte=45
+    println(age)
+
+    var phoneNumber: String ="+256787635823"
+    println(phoneNumber)
+    var weight: Float= 56.89F
+    println(weight)
+    var isStudentKenyan: Boolean= false
+    println(isStudentKenyan)
+}
